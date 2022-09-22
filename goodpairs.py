d6e08dccc00c0ce52187de273fb920ff5c825007
hiteshbanner <112494522+hiteshbanner@users.noreply.github.com>
2022-09-22 09:27:39 +0530

    Add files via upload

---
+++ goodpairs.py
@@ -0,0 +1,9 @@
+l=[int(i) for i in input().split() ]
+count=0
+for i in range(len(l)):
+    for j in range(len(l)):
+        if(l[i]==l[j]):
+            if(i<j):
+                count+=1
+                
+print(count)
---
+++ mirrorimage.py
@@ -0,0 +1,7 @@
+
+def rev(s):
+    str=s[: :-1]
+    return str
+s=input()
+print(s)
+print(rev(s))
